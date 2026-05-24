6678e686c89c5a7d286f65b3e087e7d8883081f5
Shishir Mahato <mahatoshishir6@gmail.com>
2026-05-25 01:29:29 +0530

    Generate components and configure routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Admin } from './components/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'admin', component: Admin },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
